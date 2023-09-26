ea99a6866d762ff1b606d5f103b07a77343d23a8
Danilo Dias <danilolive30@hotmail.com>
2023-09-26 14:46:58 -0300

    clear project

--- web-nextjs/src/components/Tasks/Scheduler/SchedulerReact.tsx
+++ web-nextjs/src/components/Tasks/Scheduler/SchedulerReact.tsx
@@ -1,96 +1,10 @@
-import { Dispatch, SetStateAction, memo, useEffect, useState } from "react";
-import { TextField, Button, DialogActions, Grid } from "@mui/material";
+import { Dispatch, SetStateAction, memo } from "react";
+import { Button, Grid } from "@mui/material";
 import { Scheduler } from "@aldabil/react-scheduler";
-import type {
-  ProcessedEvent,
-  SchedulerHelpers,
-} from "@aldabil/react-scheduler/types";
+import type { ProcessedEvent } from "@aldabil/react-scheduler/types";
 import EditIcon from "@mui/icons-material/Edit";
 import DeleteIcon from "@mui/icons-material/Delete";
-import { Holidays } from "@/interfaces/tasksInterface";
 
-interface CustomEditorProps {
-  scheduler: SchedulerHelpers;
-}
-const CustomEditor = ({ scheduler }: CustomEditorProps) => {
-  const event = scheduler.edited;
-
-  // Make your own form/state
-  const [state, setState] = useState({
-    title: event?.title || "",
-    description: event?.description || "",
-  });
-  const [error, setError] = useState("");
-
-  const handleChange = (value: string, name: string) => {
-    setState((prev) => {
-      return {
-        ...prev,
-        [name]: value,
-      };
-    });
-  };
-  const handleSubmit = async () => {
-    // Your own validation
-    if (state.title.length < 3) {
-      return setError("Min 3 letters");
-    }
-
-    try {
-      scheduler.loading(true);
-
-      /**Simulate remote data saving */
-      const added_updated_event = (await new Promise((res) => {
-        /**
-         * Make sure the event have 4 mandatory fields
-         * event_id: string|number
-         * title: string
-         * start: Date|string
-         * end: Date|string
-         */
-        setTimeout(() => {
-          res({
-            event_id: event?.event_id || Math.random(),
-            title: state.title,
-            start: scheduler.state.start.value,
-            end: scheduler.state.end.value,
-            description: state.description,
-          });
-        }, 3000);
-      })) as ProcessedEvent;
-
-      scheduler.onConfirm(added_updated_event, event ? "edit" : "create");
-      scheduler.close();
-    } finally {
-      scheduler.loading(false);
-    }
-  };
-  return (
-    <div>
-      <div style={{ padding: "1rem" }}>
-        <p>Load your custom form/fields</p>
-        <TextField
-          label="Title"
-          value={state.title}
-          onChange={(e) => handleChange(e.target.value, "title")}
-          error={!!error}
-          helperText={error}
-          fullWidth
-        />
-        <TextField
-          label="Description"
-          value={state.description}
-          onChange={(e) => handleChange(e.target.value, "description")}
-          fullWidth
-        />
-      </div>
-      <DialogActions>
-        <Button onClick={scheduler.close}>Cancel</Button>
-        <Button onClick={handleSubmit}>Confirm</Button>
-      </DialogActions>
-    </div>
-  );
-};
 interface Props {
   tasks: any;
   handleClickOpen: () => void;
@@ -108,7 +22,6 @@ function SchedulerReactC({
   setDeleteTask,
   holidays,
 }: Props) {
-
   const handleOpenModalEdit = (event: ProcessedEvent) => {
     setEditTask(event);
     handleClickOpen();
@@ -121,7 +34,6 @@ function SchedulerReactC({
   return (
     <Scheduler
       view="day"
-      customEditor={(scheduler) => <CustomEditor scheduler={scheduler} />}
       editable={false}
       deletable={false}
       viewerExtraComponent={(fields, event) => {
